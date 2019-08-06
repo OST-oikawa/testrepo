b6998a1b98be801bc18f1577921da3b46adb65c7
RyoheiOikawa <oikawa.ryohei@opensesame-tech.com>
2019-08-07 01:12:10 +0900

    Xamarinプロジェクト 初回登録

---
+++ CalcSample/CalcSample/CalcSample/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CalcSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // コードでボタンを作ってみる
+            Button b4 = new Button
+            {
+                Text = "4",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            Button b5 = new Button
+            {
+                Text = "5",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            Button b6 = new Button
+            {
+                Text = "6",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            // レイアウトに配置
+            middleLayout.Children.Add(b4);
+            middleLayout.Children.Add(b5);
+            middleLayout.Children.Add(b6);
+
+            // イベントをつける
+            b6.Clicked += async (sender, args) => await Navigation.PushAsync(new SecondPage());
+
+        }
+
+        async void OnButtonOneClicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new SecondPage());
+        }
+    }
+}
